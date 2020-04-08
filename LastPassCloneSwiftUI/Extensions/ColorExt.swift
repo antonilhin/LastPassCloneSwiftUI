ba21c1c2ebf03630a888c5f2f3157fe129c9a8a2
Antoni <lhin.antoni@gmail.com>
2020-04-08 20:14:36 +0700

    Project skeleton

---
+++ LastPassCloneSwiftUI/Extensions/ColorExt.swift
@@ -0,0 +1,18 @@
+//
+//  ColorExt.swift
+//  LastPassCloneSwiftUI
+//
+//  Created by Antoni on 08/04/20.
+//  Copyright © 2020 Antonilhin. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color{
+    static let accent = Color("accent")
+    static let background = Color("bg")
+    static let darkShadow = Color("darkShadow")
+    static let lightShadow = Color("lightShadow")
+    static let darkerAccent = Color("darkerAccent")
+    static let text = Color("text")
+}
